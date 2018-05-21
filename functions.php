98a95b901297bce726b85ec1b1888958fef27afe
Koray Löker <loker@radiobrecht.org>
2018-05-21 15:32:42 +0300

    Initial commit

---
+++ functions.php
@@ -0,0 +1,123 @@
+<?php
+/* WordStrap4 functions and definitions
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ * @package WordStrap4
+ */
+if ( ! function_exists( 'wordstrap4_setup' ) ) :
+	function wordstrap4_setup() {
+		load_theme_textdomain( 'wordstrap4', get_template_directory() . '/languages' );
+		add_theme_support( 'automatic-feed-links' );
+		add_theme_support( 'title-tag' );
+		add_theme_support( 'post-thumbnails' );
+		register_nav_menus( array(
+			'primary' => esc_html__( 'Primary', 'wordstrap4' ),
+		) );
+		add_theme_support( 'html5', array(
+			'search-form',
+			'comment-form',
+			'comment-list',
+			'gallery',
+			'caption',
+		) );
+
+		// Set up the WordPress core custom background feature.
+		add_theme_support( 'custom-background', apply_filters( 'wordstrap4_custom_background_args', array(
+			'default-color' => 'ffffff',
+			'default-image' => '',
+		) ) );
+
+		// Add theme support for selective refresh for widgets.
+		add_theme_support( 'customize-selective-refresh-widgets' );
+
+		/**
+		 * Add support for core custom logo.
+		 *
+		 * @link https://codex.wordpress.org/Theme_Logo
+		 */
+		add_theme_support( 'custom-logo', array(
+			'height'      => 250,
+			'width'       => 250,
+			'flex-width'  => true,
+			'flex-height' => true,
+		) );
+	}
+endif;
+add_action( 'after_setup_theme', 'wordstrap4_setup' );
+
+/**
+ * Set the content width in pixels, based on the theme's design and stylesheet.
+ *
+ * Priority 0 to make it available to lower priority callbacks.
+ *
+ * @global int $content_width
+ */
+function wordstrap4_content_width() {
+	// This variable is intended to be overruled from themes.
+	// Open WPCS issue: {@link https://github.com/WordPress-Coding-Standards/WordPress-Coding-Standards/issues/1043}.
+	// phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedVariableFound
+	$GLOBALS['content_width'] = apply_filters( 'wordstrap4_content_width', 640 );
+}
+add_action( 'after_setup_theme', 'wordstrap4_content_width', 0 );
+
+/**
+ * Register widget area.
+ *
+ * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
+ */
+function wordstrap4_widgets_init() {
+	register_sidebar( array(
+		'name'          => esc_html__( 'Sidebar', 'wordstrap4' ),
+		'id'            => 'sidebar-1',
+		'description'   => esc_html__( 'Add widgets here.', 'wordstrap4' ),
+		'before_widget' => '<section id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</section>',
+		'before_title'  => '<h2 class="widget-title">',
+		'after_title'   => '</h2>',
+	) );
+}
+add_action( 'widgets_init', 'wordstrap4_widgets_init' );
+
+// Register Custom Navigation Walker
+require_once get_template_directory() . '/class-wp-bootstrap-navwalker.php';
+
+/* Enqueue scripts and styles. */
+function wordstrap4_scripts() {
+	wp_enqueue_style( 'wordstrap4-style', get_stylesheet_uri() );
+	wp_enqueue_script( 'wordstrap4-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20180521', true );
+	wp_enqueue_script( 'wordstrap4-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20180521', true );
+	wp_enqueue_script( 'bundle', get_template_directory_uri() . '/js/bootstrap.bundle.min.js', array(), '4', true );
+	wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/js/bootstrap.min.js', array('jquery'), '4', true );
+}
+	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+		wp_enqueue_script( 'comment-reply' );
+	};
+
+add_action( 'wp_enqueue_scripts', 'wordstrap4_scripts' );
+
+/**
+ * Implement the Custom Header feature.
+ */
+require get_template_directory() . '/inc/custom-header.php';
+
+/**
+ * Custom template tags for this theme.
+ */
+require get_template_directory() . '/inc/template-tags.php';
+
+/**
+ * Functions which enhance the theme by hooking into WordPress.
+ */
+require get_template_directory() . '/inc/template-functions.php';
+
+/**
+ * Customizer additions.
+ */
+require get_template_directory() . '/inc/customizer.php';
+
+/**
+ * Load Jetpack compatibility file.
+ */
+if ( defined( 'JETPACK__VERSION' ) ) {
+	require get_template_directory() . '/inc/jetpack.php';
+}
+
---
+++ header.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * The header for our theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package WordStrap4
+ */
+
+?>
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<div id="page" class="site">
+	<a class="skip-link sr-only" href="#content"><?php esc_html_e( 'Skip to content', 'wordstrap4' ); ?></a>
+
+	<header id="masthead" class="site-header">
+		<nav class="navbar navbar-expand-md navbar-light bg-light">
+			<?php
+					wp_nav_menu( array(
+							'menu'              => 'menu',
+							'theme_location'    => 'primary',
+							'depth'             => 2,
+							'menu_class'        => 'nav',
+							'fallback_cb'       => 'wp_page_menu',
+							'walker'            => new WP_Bootstrap_Navwalker())
+					);
+			?>
+		</nav><!-- #site-navigation -->
+	</header><!-- #masthead -->
+	<div id="content" class="site-content">
