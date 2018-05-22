ab231e47d06f4067d633920e6339d2024c10c34c
Koray Löker <loker@radiobrecht.org>
2018-05-22 15:45:14 +0300

    realign columns as 9/3 instead of 8/4

--- sidebar.php
+++ sidebar.php
@@ -3,7 +3,7 @@
 		return;
 	}
 ?>
-		<aside class="col-md-4 blog-sidebar">
+		<aside class="col-md-3 blog-sidebar">
 			<?php dynamic_sidebar( 'sidebar-1' ); ?>
 		</aside><!-- #secondary -->
 	</div>
--- single.php
+++ single.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 <main id="main" role="main" class="container">
 	<div class="row">
-		 <div class="col-md-8 blog-main">
+		 <div class="col-md-9 blog-main">
 			<?php
 			if ( have_posts() ) :
 
@@ -14,15 +14,15 @@
 				endif;
 				while ( have_posts() ) :
 					the_post();
-					get_template_part( 'template-parts/content', get_post_type() );
+        	get_template_part( 'template-parts/content', get_post_type() );
 				endwhile;
 				the_posts_navigation();
 			else :
 				get_template_part( 'template-parts/content', 'none' );
 			endif;
 			?>
-		</div>
-	
+		</div><!---->
+
 <?php
 get_sidebar();
 get_footer();
