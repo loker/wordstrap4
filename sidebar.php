9d6c3380f758b2ac68e9827a7680fa52db03e59c
Koray Löker <loker@radiobrecht.org>
2018-05-22 14:44:05 +0300

    get sidebar with bootstrap layout

--- sidebar.php
+++ sidebar.php
@@ -1,17 +1,10 @@
 <?php
-/**
- * The sidebar containing the main widget area
- *
- * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
- *
- * @package WordStrap4
- */
-
-if ( ! is_active_sidebar( 'sidebar-1' ) ) {
-	return;
-}
+	if ( ! is_active_sidebar( 'sidebar-1' ) ) {
+		return;
+	}
 ?>
-
-<aside id="secondary" class="widget-area">
-	<?php dynamic_sidebar( 'sidebar-1' ); ?>
-</aside><!-- #secondary -->
+		<aside class="col-md-4 blog-sidebar">
+			<?php dynamic_sidebar( 'sidebar-1' ); ?>
+		</aside><!-- #secondary -->
+	</div>
+</main><!-- /.container -->
--- single.php
+++ single.php
@@ -1,37 +1,28 @@
-<?php
-/**
- * The template for displaying all single posts
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
- *
- * @package WordStrap4
- */
+<?php get_header(); ?>
+<main id="main" role="main" class="container">
+	<div class="row">
+		 <div class="col-md-8 blog-main">
+			<?php
+			if ( have_posts() ) :
 
-get_header();
-?>
-
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
-
-		<?php
-		while ( have_posts() ) :
-			the_post();
-
-			get_template_part( 'template-parts/content', get_post_type() );
-
-			the_post_navigation();
-
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
+				if ( is_home() && ! is_front_page() ) :
+					?>
+				<header>
+					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+				</header>
+					<?php
+				endif;
+				while ( have_posts() ) :
+					the_post();
+					get_template_part( 'template-parts/content', get_post_type() );
+				endwhile;
+				the_posts_navigation();
+			else :
+				get_template_part( 'template-parts/content', 'none' );
 			endif;
-
-		endwhile; // End of the loop.
-		?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
+			?>
+		</div>
+	
 <?php
 get_sidebar();
 get_footer();
