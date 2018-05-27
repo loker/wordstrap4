bc09a50bfd835193aa5d94e208ec451b83b21e25
Koray Löker <loker@radiobrecht.org>
2018-05-27 19:26:23 +0300

    no summary

--- sidebar.php
+++ sidebar.php
@@ -8,8 +8,7 @@
 			<?php $my_query = new WP_Query('category_name=program&showposts=1'); ?>
 			<?php while ($my_query->have_posts()) : $my_query->the_post(); ?>
 				<a href="<?php the_permalink() ?>" title="<?php the_title(); ?>">
-					<?php the_title(); ?> programı
-					<?php the_content('', TRUE ); ?>
+					<?php the_title(); ?> programı için tıklayın
 				</a>
 			</section>
 			<?php endwhile; ?>
