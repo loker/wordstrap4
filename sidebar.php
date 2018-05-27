b14488843e545ce311fc694a30a069064b9bb78f
Koray Löker <loker@radiobrecht.org>
2018-05-28 00:07:08 +0300

    get program link smaller

--- sidebar.php
+++ sidebar.php
@@ -4,7 +4,7 @@
 	}
 ?>
 		<aside class="col-md-3 blog-sidebar">
-			<section class="widget mt-3">
+			<section class="program widget mt-3">
 			<?php $my_query = new WP_Query('category_name=program&showposts=1'); ?>
 			<?php while ($my_query->have_posts()) : $my_query->the_post(); ?>
 				<a href="<?php the_permalink() ?>" title="<?php the_title(); ?>">
