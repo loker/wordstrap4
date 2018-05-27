892f62ea2ec58ae5bb8613ebc51843d9d2e48d30
Koray Löker <loker@radiobrecht.org>
2018-05-27 19:08:18 +0300

    remove category from all

--- archive.php
+++ archive.php
@@ -7,7 +7,7 @@
 
 			<header class="page-header">
 				<?php
-				the_archive_title( '<h1 class="page-title">', '</h1>' );
+				single_term_title( '<h1 class="page-title">', '</h1>' );
 				the_archive_description( '<div class="archive-description">', '</div>' );
 				?>
 			</header><!-- .page-header -->
